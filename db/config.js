cdfc754b2fd338fe199b5dac6e84e631bbac4c35
Julio <juliobermudezch@gmail.com>
2022-05-05 13:00:41 -0300

    commit

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 const pool = mysql.createPool({
     host:'us-cdbr-east-05.cleardb.net',
-    user:'b1ab3693220e6b',
+    user:'b1ab3693220e6b', 
     password:'dbad1e4f',
     database:'heroku_40673a1facac44b',
     connectionLimit:10,
